a5b88d37ea5da0702771e2bc453edc174d3a23b9
rmalcac <rmalcac16_1@unc.edu.pe>
2023-11-21 11:18:37 -0500

    Add GoogleAnalitycs

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -28,6 +28,19 @@
 
     <script src="{{ asset('web/js/jquery-3.7.0.min.js') }}"></script>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-S1ZQL85DRY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-S1ZQL85DRY');
+    </script>
+
     @yield('aditionals')
 
     <script src="{{ asset('web/js/popAds.js') }}"></script>
